a19bcb204e6632085a153587a91fe7aaaa589560
Хамелеон <maxali946@gmail.com>
2018-11-14 18:57:05 +0400

    Add project files.

---
+++ ToDoListWebServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListWebServer.Server;
+
+namespace ToDoListWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDoWebServer webServer = new ToDoWebServer("127.0.0.1", 5600);
+
+            webServer.Start();
+
+            Console.WriteLine("Web server start...");
+
+            try
+            {
+                webServer.Listen();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Web server stopped...");
+                Console.WriteLine(ex.Message);
+                webServer.Stop();
+            }
+        }
+    }
+}
